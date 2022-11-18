1bb8f06b3b0e879fefa7535e301d35808f409d24
Toledo <rextoldz@gmail.com>
2022-11-18 15:57:35 +0800

    added new

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.home')
 @section('content')
         <!-- Background Video-->
-        <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="{{ asset('vdo/slsubc.mp4')}}" type="video/mp4" /></video>
+        <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="{{ asset('/vdo/slsubc.mp4')}}" type="video/mp4" /></video>
         <!-- Masthead-->
         <div class="container-fluid" style="height: inherit;">
             <div class="row" style="height: inherit;">
